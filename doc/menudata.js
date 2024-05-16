44f981b002909f04aaf5e545f73bb3b103a13076
Guilherme Noronha <xpto7.pt@gmail.com>
2024-05-17 00:49:18 +0100

    doxygen

--- doc/menudata.js
+++ doc/menudata.js
@@ -89,7 +89,8 @@ var menudata={children:[
 {text:"v",url:"functions_vars.html#index_v"},
 {text:"w",url:"functions_vars.html#index_w"},
 {text:"x",url:"functions_vars.html#index_x"},
-{text:"y",url:"functions_vars.html#index_y"}]}]}]},
+{text:"y",url:"functions_vars.html#index_y"}]},
+{text:"Enumerations",url:"functions_enum.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
